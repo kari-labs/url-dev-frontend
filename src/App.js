62cdffec50092283d641c6e8e486341e07e54b68
Ian Fabs <ianfabs@outlook.com>
2019-09-03 23:14:04 -0400

    Fixed linting issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Grommet from "./components/Grommet";
 import Header from "./components/Header";
